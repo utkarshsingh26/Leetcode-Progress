5c24af56457e0dd9ebb2f3af0cbdcf499832c521
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2025-10-07 11:58:34 -0700

    Time: 3 ms (87.74%), Space: 19.2 MB (51.08%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,35 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        graph = defaultdict(list)
+
+        for u,v in prerequisites:
+            graph[v].append(u)
+        
+        UNVISITED = 0
+        VISITING = 1
+        VISITED = 2
+
+        status = [UNVISITED] * numCourses
+
+        def cycle(node):
+            if status[node] == VISITED:
+                return False
+            elif status[node] == VISITING:
+                return True
+            else:
+                status[node] = VISITING
+                for neighbor in graph[node]:
+                    if cycle(neighbor):
+                        return True
+                status[node] = VISITED
+                return False
+        
+        for i in range(numCourses):
+            if cycle(i):
+                return False
+        
+        return True
+
+        
+        

3504c1411581bc58b002addeeb7b67173199754c
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2025-10-07 12:06:52 -0700

    Time: 3 ms (75.8%), Space: 19.5 MB (35.03%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        graph = defaultdict(list)
+
+        for u,v in prerequisites:
+            graph[v].append(u)
+        
+        UNVISITED = 0
+        VISITING = 1
+        VISITED = 2
+
+        status = [UNVISITED] * numCourses
+        result = []
+
+        def cycle(node):
+            if status[node] == VISITED:
+                return False
+            elif status[node] == VISITING:
+                return True
+            else:
+                status[node] = VISITING
+                for neighbor in graph[node]:
+                    if cycle(neighbor):
+                        return True
+                status[node] = VISITED
+                result.append(node)
+                return False
+        
+        for i in range(numCourses):
+            if cycle(i):
+                return []
+        
+        return result[::-1]

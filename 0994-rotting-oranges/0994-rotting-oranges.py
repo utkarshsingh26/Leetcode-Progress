198e19b8c0ad693156ea32f511925eda00de7d10
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2025-10-21 11:47:36 -0700

    Time: 3 ms (81.53%), Space: 17.7 MB (71.54%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -5,24 +5,21 @@ class Solution:
         rows = len(grid)
         columns = len(grid[0])
 
-        queue = deque()
         fresh = [0]
+        queue = deque()
 
         for r in range(rows):
             for c in range(columns):
-                if grid[r][c] == 2:
-                    queue.append((r,c))
-                elif grid[r][c] == 1:
+                if grid[r][c] == 1:
                     fresh[0] += 1
+                elif grid[r][c] == 2:
+                    queue.append((r,c))
         
-        if fresh[0] == 0:
+        if not queue and fresh[0] == 0:
             return 0
         
-        if not queue:
-            return -1
-            
         directions = [(0,1), (0,-1), (1,0), (-1,0)]
-        
+
         def bfs(queue):
             minutes = 0
             while queue:
@@ -31,19 +28,19 @@ class Solution:
 
                 for _ in range(level_length):
                     row, column = queue.popleft()
-
+                    
                     for dr, dc in directions:
                         nr, nc = row + dr, column + dc
 
                         if 0 <= nr < rows and 0 <= nc < columns and grid[nr][nc] == 1:
-                            fresh[0] -= 1
                             grid[nr][nc] = 2
                             queue.append((nr,nc))
+                            fresh[0] -= 1
                 
                 minutes += 1
 
             return minutes-1 if fresh[0] == 0 else -1
-
         
         return bfs(queue)
+
         

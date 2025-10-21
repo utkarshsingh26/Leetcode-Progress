60f5725583a99b1d80393548b36faa30cee73a18
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2025-10-21 12:03:03 -0700

    Time: 11 ms (28.27%), Space: 21.8 MB (58.1%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -4,6 +4,7 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+
         rows = len(board)
         columns = len(board[0])
 
@@ -13,29 +14,26 @@ class Solution:
         for r in range(rows):
             for c in range(columns):
                 if (r == 0 or r == rows-1 or c == 0 or c == columns-1) and board[r][c] == "O":
-                    visited.add((r,c))
                     queue.append((r,c))
+                    visited.add((r,c))
         
-        def bfs(queue):
+        directions = [(0,1), (0,-1), (1,0), (-1,0)]
+
+        def bfs(queue, visited):
             while queue:
                 row, column = queue.popleft()
-                directions = [(0,1), (0,-1), (1,0), (-1,0)]
 
                 for dr, dc in directions:
                     nr, nc = row + dr, column + dc
-                    
-                    if 0 <= nr < rows and 0 <=nc < columns and board[nr][nc] == "O" and (nr, nc) not in visited:
-                        visited.add((nr,nc))
+
+                    if 0<= nr < rows and 0 <= nc < columns and board[nr][nc] == "O" and (nr,nc) not in visited:
                         queue.append((nr,nc))
+                        visited.add((nr,nc))
         
-        bfs(queue)
+        bfs(queue, visited)
 
         for r in range(rows):
             for c in range(columns):
-                if board[r][c] == "O" and (r,c) not in visited:
+                if not(r == 0 or r == rows-1 or c == 0 or c == columns-1) and (r,c) not in visited:
                     board[r][c] = "X"
-        
-
-
-
         

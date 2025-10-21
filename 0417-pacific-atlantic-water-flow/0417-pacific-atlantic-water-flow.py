204b5b61bcdef97266ffe790b58002cc4af979ef
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2025-10-21 11:55:29 -0700

    Time: 22 ms (93.01%), Space: 19.3 MB (57.14%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,46 +1,39 @@
 from collections import deque
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-
-        pacific_seen = set()
-        pacific_queue = deque()
-
-        atlantic_seen = set()
-        atlantic_queue = deque()
-
+        
         rows = len(heights)
         columns = len(heights[0])
 
-        for c in range(columns):
-            pacific_seen.add((0,c))
-            pacific_queue.append((0,c))
-        
+        atlantic_queue = deque()
+        atlantic_set = set()
+
+        pacific_queue = deque()
+        pacific_set = set()
+
         for r in range(rows):
-            pacific_seen.add((r,0))
-            pacific_queue.append((r,0))
-        
-        for c in range(columns):
-            atlantic_seen.add((rows-1,c))
-            atlantic_queue.append((rows-1,c))
-        
-        for r in range(rows):
-            atlantic_seen.add((r,columns-1))
-            atlantic_queue.append((r,columns-1))
-        
+            for c in range(columns):
+                if (r == 0 or c == 0):
+                    pacific_queue.append((r,c))
+                    pacific_set.add((r,c))
+                if (r == rows-1 or c == columns-1):
+                    atlantic_queue.append((r,c))
+                    atlantic_set.add((r,c))
+
+        directions = [(0,1), (0,-1), (1,0), (-1,0)]
+
         def bfs(queue, visited):
-            directions = [(0,1), (1,0), (0,-1), (-1,0)]
             while queue:
                 row, column = queue.popleft()
 
                 for dr, dc in directions:
-                    nr, nc = dr + row, dc + column
+                    nr, nc = row + dr, column + dc
 
-                    if 0 <= nr < rows and 0 <= nc < columns and heights[nr][nc] >= heights[row][column] and (nr, nc) not in visited:
-                        visited.add((nr,nc))
+                    if 0 <= nr < rows and 0 <= nc < columns and heights[nr][nc] >= heights[row][column] and (nr,nc) not in visited:
                         queue.append((nr,nc))
-
+                        visited.add((nr,nc))
         
-        bfs(atlantic_queue, atlantic_seen)
-        bfs(pacific_queue, pacific_seen)
+        bfs(pacific_queue, pacific_set)
+        bfs(atlantic_queue, atlantic_set)
 
-        return list(atlantic_seen & pacific_seen)
+        return list(pacific_set & atlantic_set)

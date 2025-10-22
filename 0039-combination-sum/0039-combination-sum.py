cad4650b775d60583c5074453a873c437855e4ef
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2025-10-22 11:04:38 -0700

    Time: 10 ms (60.97%), Space: 18.1 MB (19.62%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        
+
         result = []
         temp = []
 
@@ -15,9 +15,8 @@ class Solution:
             backtrack(index+1, curr_sum)
 
             temp.append(candidates[index])
-            backtrack(index, candidates[index] + curr_sum)
+            backtrack(index, curr_sum + candidates[index])
             temp.pop()
-
         
         backtrack(0,0)
         return result

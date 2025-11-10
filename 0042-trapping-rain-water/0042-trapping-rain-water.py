2f9c840283406ced95836e3d0e52b9060fe95cfe
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2025-11-10 13:56:47 -0700

    Time: 29 ms (22.33%), Space: 19.4 MB (18.65%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -3,8 +3,7 @@ class Solution:
         
         left = [0] * len(height)
         right = [0] * len(height)
-        output = [0] * len(height)
-        result = 0
+        water = 0
 
         left[0] = height[0]
         right[-1] = height[-1]
@@ -16,9 +15,6 @@ class Solution:
             right[i] = max(right[i+1], height[i])
         
         for i in range(len(height)):
-            output[i] = min(left[i], right[i]) - height[i]
-            result += output[i]
+            water += min(left[i], right[i]) - height[i]
         
-        return result
-        
-        
+        return water

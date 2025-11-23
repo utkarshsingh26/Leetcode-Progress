63208db21a883150ad12323e1f2343d1f1865bd1
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2025-11-23 14:28:26 -0700

    Time: 3 ms (81.41%), Space: 17.7 MB (70.7%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,12 +1,14 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-
+        
         rows = len(grid)
         columns = len(grid[0])
 
-        fresh = [0]
         queue = deque()
+        fresh = [0]
+        visited = set()
+        directions = [(0,1), (0,-1), (1,0), (-1,0)]
 
         for r in range(rows):
             for c in range(columns):
@@ -15,32 +17,26 @@ class Solution:
                 elif grid[r][c] == 2:
                     queue.append((r,c))
         
-        if not queue and fresh[0] == 0:
+        if fresh[0] == 0:
             return 0
         
-        directions = [(0,1), (0,-1), (1,0), (-1,0)]
-
-        def bfs(queue):
+        def bfs(queue, visted):
             minutes = 0
-            while queue:
-                level = []
-                level_length = len(queue)
 
-                for _ in range(level_length):
+            while queue:
+                for _ in range(len(queue)):
                     row, column = queue.popleft()
-                    
+
                     for dr, dc in directions:
                         nr, nc = row + dr, column + dc
 
-                        if 0 <= nr < rows and 0 <= nc < columns and grid[nr][nc] == 1:
-                            grid[nr][nc] = 2
-                            queue.append((nr,nc))
+                        if 0 <= nr < rows and 0 <= nc < columns and grid[nr][nc] == 1 and (nr, nc) not in visited:
                             fresh[0] -= 1
-                
+                            grid[nr][nc] == 2
+                            visited.add((nr,nc))
+                            queue.append((nr,nc))
                 minutes += 1
 
             return minutes-1 if fresh[0] == 0 else -1
         
-        return bfs(queue)
-
-        
+        return bfs(queue, visited)

4fd27026a80d1577dfa06cebe99a647add19ffd9
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-01-05 11:33:45 -0700

    Time: 1117 ms (14.31%), Space: 20.7 MB (55.31%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -7,11 +7,13 @@ class Solution:
         for i in range(len(nums)-2):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
+
             left = i+1
             right = len(nums)-1
 
             while left < right:
                 if nums[i] + nums[left] + nums[right] == 0:
+
                     temp = [nums[i], nums[left], nums[right]]
                     result.append(temp)
 
@@ -20,6 +22,7 @@ class Solution:
 
                     while left < right and nums[left] == nums[left-1]:
                         left += 1
+                    
                     while left < right and nums[right] == nums[right+1]:
                         right -= 1
 
@@ -27,5 +30,5 @@ class Solution:
                     left += 1
                 elif nums[i] + nums[left] + nums[right] > 0:
                     right -= 1
-            
+        
         return result

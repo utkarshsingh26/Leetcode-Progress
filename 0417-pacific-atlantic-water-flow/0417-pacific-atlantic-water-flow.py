9aa6fbaf4cc00a88580781f5abcd31e9a6af3970
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-01-07 22:16:03 -0700

    Time: 23 ms (93.3%), Space: 20.9 MB (5.29%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -5,34 +5,41 @@ class Solution:
         rows = len(heights)
         columns = len(heights[0])
 
-        atlantic_queue = deque()
-        atlantic_set = set()
-
-        pacific_queue = deque()
         pacific_set = set()
+        atlantic_set = set()
+        pacific_queue = deque()
+        atlantic_queue = deque()
 
+        for c in range(columns):
+            pacific_queue.append((0,c))
+            pacific_set.add((0,c))
+            
         for r in range(rows):
-            for c in range(columns):
-                if (r == 0 or c == 0):
-                    pacific_queue.append((r,c))
-                    pacific_set.add((r,c))
-                if (r == rows-1 or c == columns-1):
-                    atlantic_queue.append((r,c))
-                    atlantic_set.add((r,c))
-
-        directions = [(0,1), (0,-1), (1,0), (-1,0)]
-
+            pacific_queue.append((r,0))
+            pacific_set.add((r,0))
+        
+        for c in range(columns):
+            atlantic_queue.append((rows-1,c))
+            atlantic_set.add((rows-1,c))
+        
+        for r in range(rows):
+            atlantic_queue.append((r, columns-1))
+            atlantic_set.add((r, columns-1))
+        
         def bfs(queue, visited):
+
+            directions = [(0,1), (0,-1), (1,0), (-1,0)]
+
             while queue:
                 row, column = queue.popleft()
 
                 for dr, dc in directions:
                     nr, nc = row + dr, column + dc
 
-                    if 0 <= nr < rows and 0 <= nc < columns and heights[nr][nc] >= heights[row][column] and (nr,nc) not in visited:
-                        queue.append((nr,nc))
+                    if 0 <= nr < rows and 0 <= nc < columns and (nr, nc) not in visited and heights[nr][nc] >= heights[row][column]:
                         visited.add((nr,nc))
-        
+                        queue.append((nr,nc))
+
         bfs(pacific_queue, pacific_set)
         bfs(atlantic_queue, atlantic_set)
 

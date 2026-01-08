8abb045c9d27b2d5b76f954dfe621fe72aaca3f5
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-01-07 22:41:07 -0700

    Time: 7 ms (56.7%), Space: 22.3 MB (19.48%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -4,35 +4,33 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        
         rows = len(board)
         columns = len(board[0])
-
+        queue = deque()
         visited = set()
-        
-        def bfs(r,c):
-            queue = deque()
-            queue.append((r,c))
-            visited.add((r,c))
 
+        for r in range(rows):
+            for c in range(columns):
+                if board[r][c] == "O" and (r == 0 or r == rows-1 or c == 0 or c == columns-1):
+                    queue.append((r,c))
+
+        def bfs(queue):
             directions = [(0,1), (0,-1), (1,0), (-1,0)]
 
             while queue:
                 row, column = queue.popleft()
 
-                for dr,dc in directions:
-                    nr, nc = dr + row, dc + column
+                for dr, dc in directions:
+                    nr, nc = row + dr, column + dc
 
-                    if 0 <= nr < rows and 0 <= nc < columns and board[nr][nc] == "O" and (nr,nc) not in visited:
+                    if 0 <= nr < rows and 0 <= nc < columns and board[nr][nc] == "O" and (nr,nc) not in visited :
+
                         visited.add((nr,nc))
                         queue.append((nr,nc))
+        
+        bfs(queue)
 
         for r in range(rows):
             for c in range(columns):
-                if board[r][c] == "O" and (r == 0 or r == rows-1 or c == 0 or c == columns-1):
-                    bfs(r,c)
-        
-        for r in range(rows):
-            for c in range(columns):
-                if board[r][c] == "O" and (r,c) not in visited and not (r == 0 or r == rows-1 or c == 0 or c == columns-1):
+                if not (r == 0 or r == rows-1 or c == 0 or c == columns-1) and (r,c) not in visited:
                     board[r][c] = "X"

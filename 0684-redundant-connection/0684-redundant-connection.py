0e4822838981b74950f673eaa63a5fb267dae511
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-01-08 21:25:26 -0700

    Time: 12 ms (9.8%), Space: 20.2 MB (5.33%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -2,28 +2,25 @@ class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         
         graph = defaultdict(list)
-
         visited = set()
 
         def cycle(node1, node2):
             if node1 == node2:
                 return True
             
-            visited.add(node1)
+            visited.add(node2)
 
-            for neighbor in graph[node1]:
+            for neighbor in graph[node2]:
                 if neighbor not in visited:
-                    if cycle(neighbor, node2):
+                    if cycle(node1, neighbor):
                         return True
             
             return False
 
         for u,v in edges:
-            if u in graph and v in graph:
+            if u in graph or v in graph:
                 visited.clear()
                 if cycle(u,v):
                     return [u,v]
             graph[u].append(v)
             graph[v].append(u)
-        
-        return []

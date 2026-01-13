b39d8411286add3e8985e3f20ef359d8b44c61c7
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-01-12 18:42:32 -0700

    Time: 0 ms (100%), Space: 19.6 MB (6.69%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -4,22 +4,22 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
         
-        def output(nums):
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
 
-            if len(nums) == 1:
-                return nums[0]
-                
-            dp = [0] * len(nums)
+        dp_first =[0] * (len(nums)-1)
+        dp_second = [0] * (len(nums)-1)
 
-            dp[0] = nums[0]
-            dp[1] = max(nums[0], nums[1])
+        dp_first[0] = nums[0]
+        dp_first[1] = max(nums[0], nums[1])
 
-            for i in range(2, len(nums)):
-                dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        for i in range(2, len(dp_first)):
+            dp_first[i] = max(dp_first[i-2] + nums[i], dp_first[i-1])
+        
+        dp_second[0] = nums[1]
+        dp_second[1] = max(nums[1], nums[2])
 
-            return dp[-1]
-
-        exclude = output(nums[1:])
-        include = output(nums[:-1])
-
-        return max(include, exclude) 
+        for i in range(2, len(dp_second)):
+            dp_second[i] = max(dp_second[i-2] + nums[i+1], dp_second[i-1])
+        
+        return max(dp_first[-1], dp_second[-1])

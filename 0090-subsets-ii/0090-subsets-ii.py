cbe3590602434df8e049fb5e445c80625e49dff4
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-01-12 23:09:54 -0700

    Time: 0 ms (100%), Space: 19.6 MB (6.24%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-
+        
         result = set()
         temp = []
         nums.sort()
@@ -8,16 +8,15 @@ class Solution:
         result.add(tuple(temp[:]))
 
         def backtrack(index):
-            if index >= len(nums):
+            if index >= len(nums) or len(temp) == len(nums):
                 return
             
             backtrack(index+1)
 
             temp.append(nums[index])
             result.add(tuple(temp[:]))
-
             backtrack(index+1)
             temp.pop()
         
         backtrack(0)
-        return [list(t) for t in result]
+        return list(result)

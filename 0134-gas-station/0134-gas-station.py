d143b3fd420862d087bb289d97ab8904414b785e
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-01-17 12:38:13 -0700

    Time: 19 ms (71.91%), Space: 26.2 MB (9.33%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -5,14 +5,13 @@ class Solution:
             return -1
         
         station = 0
-        difference = 0
+        diff = 0
 
         for i in range(len(gas)):
+            diff += gas[i] - cost[i]
 
-            difference += gas[i] - cost[i]
-
-            if difference < 0:
-                difference = 0
+            if diff < 0:
                 station = i + 1
+                diff = 0
         
         return station

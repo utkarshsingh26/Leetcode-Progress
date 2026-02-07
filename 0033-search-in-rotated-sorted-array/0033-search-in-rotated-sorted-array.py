615526f6d34a5ebef4ad11ea768af8767dbcdf0e
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-02-06 20:34:01 -0700

    Time: 0 ms (100%), Space: 19.6 MB (22.73%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -3,49 +3,44 @@ class Solution:
 
         def binarySearch(nums, target):
             left = 0
-            right = len(nums) - 1
+            right = len(nums)-1
 
             while left <= right:
-                mid = (left + right) // 2
+                mid = (left + right)//2
 
                 if nums[mid] == target:
                     return mid
                 elif nums[mid] < target:
                     left = mid + 1
-                elif nums[mid] > target:
+                else:
                     right = mid - 1
             
             return -1
         
         left = 0
-        right = len(nums) - 1
+        right = len(nums)-1
 
         while left < right:
-            mid = (left + right) // 2
+            mid = (left + right)//2
 
             if nums[mid] > nums[right]:
                 left = mid + 1
-            elif nums[mid] < nums[right]:
+            else:
                 right = mid
         
-        minNum = nums[left]
         minIndex = left
+        minElement = nums[minIndex]
 
-        if target == minNum:
+        if target == minElement:
             return minIndex
         
         if minIndex == 0:
             return binarySearch(nums, target)
         else:
-            
             if nums[0] <= target <= nums[minIndex-1]:
                 arr = nums[:minIndex]
                 return binarySearch(arr, target)
-            elif nums[minIndex] <= target <= nums[-1]:
-                arr = nums[minIndex:]
-                result = binarySearch(arr, target)
-                return result + minIndex if result != -1 else -1
             else:
-                return -1
-
-                
+                arr = nums[minIndex:]
+                answer = binarySearch(arr, target)
+                return -1 if answer == -1 else answer + minIndex

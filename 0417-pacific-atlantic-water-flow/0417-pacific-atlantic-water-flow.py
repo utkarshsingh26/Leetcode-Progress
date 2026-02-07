bd2900ed1250d52053d6781950c7f3116395a82c
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-02-07 12:40:20 -0700

    Time: 21 ms (92.79%), Space: 20.8 MB (50.05%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -6,41 +6,41 @@ class Solution:
         columns = len(heights[0])
 
         pacific_set = set()
-        atlantic_set = set()
         pacific_queue = deque()
+
+        atlantic_set = set()
         atlantic_queue = deque()
 
-        for c in range(columns):
-            pacific_queue.append((0,c))
-            pacific_set.add((0,c))
-            
         for r in range(rows):
-            pacific_queue.append((r,0))
             pacific_set.add((r,0))
+            pacific_queue.append((r,0))
         
         for c in range(columns):
-            atlantic_queue.append((rows-1,c))
-            atlantic_set.add((rows-1,c))
+            pacific_set.add((0,c))
+            pacific_queue.append((0,c))
         
         for r in range(rows):
-            atlantic_queue.append((r, columns-1))
             atlantic_set.add((r, columns-1))
+            atlantic_queue.append((r, columns-1))
         
+        for c in range(columns):
+            atlantic_set.add((rows-1, c))
+            atlantic_queue.append((rows-1, c))
+
         def bfs(queue, visited):
-
             directions = [(0,1), (0,-1), (1,0), (-1,0)]
-
             while queue:
                 row, column = queue.popleft()
-
                 for dr, dc in directions:
                     nr, nc = row + dr, column + dc
 
-                    if 0 <= nr < rows and 0 <= nc < columns and (nr, nc) not in visited and heights[nr][nc] >= heights[row][column]:
+                    if 0 <= nr < rows and 0 <= nc < columns and heights[nr][nc] >= heights[row][column] and (nr, nc) not in visited:
+                        queue.append((nr, nc))
                         visited.add((nr,nc))
-                        queue.append((nr,nc))
-
+        
         bfs(pacific_queue, pacific_set)
         bfs(atlantic_queue, atlantic_set)
 
         return list(pacific_set & atlantic_set)
+
+

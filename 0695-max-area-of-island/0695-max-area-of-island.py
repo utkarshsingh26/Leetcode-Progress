b2ffc5d6616e4f4698714b6864129f93e7e51320
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-02-07 12:21:12 -0700

    Time: 23 ms (49.16%), Space: 19.8 MB (43.92%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -4,25 +4,24 @@ class Solution:
         
         rows = len(grid)
         columns = len(grid[0])
-        maxArea = 0
+        max_area = 0
         visited = set()
 
         def bfs(r,c):
+            area = 1
             queue = deque()
             queue.append((r,c))
             visited.add((r,c))
             directions = [(0,1), (0,-1), (1,0), (-1,0)]
-            area = 1
 
             while queue:
                 row, column = queue.popleft()
-                
                 for dr, dc in directions:
                     nr, nc = row + dr, column + dc
 
-                    if 0 <= nr < rows and 0 <= nc < columns and grid[nr][nc] == 1 and (nr,nc) not in visited:
-                        visited.add((nr,nc))
-                        queue.append((nr,nc))
+                    if 0 <= nr < rows and 0 <= nc < columns and grid[nr][nc] == 1 and (nr, nc) not in visited:
+                        queue.append((nr, nc))
+                        visited.add((nr, nc))
                         area += 1
             
             return area
@@ -31,6 +30,6 @@ class Solution:
             for c in range(columns):
                 if grid[r][c] == 1 and (r,c) not in visited:
                     area = bfs(r,c)
-                    maxArea = max(area, maxArea)
+                    max_area = max(max_area, area)
         
-        return maxArea
+        return max_area

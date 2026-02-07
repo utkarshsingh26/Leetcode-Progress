3bc05ba0a6bde694ee27cc4e889afff40a0d8aca
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-02-06 20:27:56 -0700

    Time: 0 ms (100%), Space: 19.5 MB (37.7%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -2,10 +2,10 @@ class Solution:
     def findMin(self, nums: List[int]) -> int:
         
         left = 0
-        right = len(nums) - 1
+        right = len(nums)-1
 
         while left < right:
-            mid = (left + right) // 2
+            mid = (left + right)//2
 
             if nums[mid] > nums[right]:
                 left = mid + 1

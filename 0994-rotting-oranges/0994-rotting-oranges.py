80febde65d48dd54131935bb74639dbb1f3cb12b
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-02-07 18:17:22 -0700

    Time: 3 ms (78.56%), Space: 19.5 MB (24.71%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -4,39 +4,36 @@ class Solution:
         
         rows = len(grid)
         columns = len(grid[0])
-
         queue = deque()
         fresh = [0]
-        visited = set()
-        directions = [(0,1), (0,-1), (1,0), (-1,0)]
 
         for r in range(rows):
             for c in range(columns):
-                if grid[r][c] == 1:
-                    fresh[0] += 1
-                elif grid[r][c] == 2:
+                if grid[r][c] == 2:
                     queue.append((r,c))
+                elif grid[r][c] == 1:
+                    fresh[0] += 1
         
         if fresh[0] == 0:
             return 0
         
-        def bfs(queue, visted):
+        def bfs(queue):
             minutes = 0
+            directions = [(0,1), (0,-1), (1,0), (-1,0)]
 
             while queue:
-                for _ in range(len(queue)):
+                level_length = len(queue)
+                for _ in range(level_length):
                     row, column = queue.popleft()
-
                     for dr, dc in directions:
                         nr, nc = row + dr, column + dc
 
-                        if 0 <= nr < rows and 0 <= nc < columns and grid[nr][nc] == 1 and (nr, nc) not in visited:
+                        if 0 <= nr < rows and 0 <= nc < columns and grid[nr][nc] == 1:
+                            grid[nr][nc] = 2
                             fresh[0] -= 1
-                            grid[nr][nc] == 2
-                            visited.add((nr,nc))
-                            queue.append((nr,nc))
+                            queue.append((nr, nc))
                 minutes += 1
-
+            
             return minutes-1 if fresh[0] == 0 else -1
         
-        return bfs(queue, visited)
+        return bfs(queue)

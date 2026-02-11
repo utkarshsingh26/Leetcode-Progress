07490df4728877d6aa2434144040b4d03b7dca02
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-02-11 14:46:51 -0700

    Time: 0 ms (100%), Space: 19.4 MB (20.32%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -6,20 +6,23 @@ class Solution:
         
         if len(nums) == 2:
             return max(nums[0], nums[1])
-
-        dp_first =[0] * (len(nums)-1)
-        dp_second = [0] * (len(nums)-1)
-
-        dp_first[0] = nums[0]
-        dp_first[1] = max(nums[0], nums[1])
-
-        for i in range(2, len(dp_first)):
-            dp_first[i] = max(dp_first[i-2] + nums[i], dp_first[i-1])
         
-        dp_second[0] = nums[1]
-        dp_second[1] = max(nums[1], nums[2])
+        def robbing(nums):
 
-        for i in range(2, len(dp_second)):
-            dp_second[i] = max(dp_second[i-2] + nums[i+1], dp_second[i-1])
+            dp = [0] * len(nums)
+            dp[0] = nums[0]
+            dp[1] = max(nums[0], nums[1])
+
+            for i in range(2, len(dp)):
+                dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+            
+            return dp[-1]
         
-        return max(dp_first[-1], dp_second[-1])
+        length_of_houses = len(nums)
+
+        scenario1 = robbing(nums[1:])
+        scenario2 = robbing(nums[:length_of_houses-1])
+
+        return max(scenario1, scenario2)
+
+

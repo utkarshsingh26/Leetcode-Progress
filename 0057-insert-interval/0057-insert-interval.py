584e5d28d77f5573004e682023904980da7edc00
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-02-11 13:53:30 -0700

    Time: 0 ms (100%), Space: 21.4 MB (55.04%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -4,14 +4,13 @@ class Solution:
         intervals = intervals + [newInterval]
         intervals.sort(key=lambda x:x[0])
         result = []
-        result.append(intervals[0])
 
-        for interval in intervals[1:]:
-
+        for interval in intervals:
+            if not result or result[-1][1] < interval[0]:
+                result.append(interval)
+            
             if interval[0] <= result[-1][1]:
                 result[-1][0] = min(result[-1][0], interval[0])
                 result[-1][1] = max(result[-1][1], interval[1])
-            else:
-                result.append(interval)
-        
-        return result
+
+        return result 

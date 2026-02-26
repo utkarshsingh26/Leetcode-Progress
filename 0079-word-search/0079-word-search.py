ec170eb78b31e27dbeb612743a788fe67a5765e4
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-02-25 23:44:43 -0700

    Time: 3503 ms (68.55%), Space: 19.5 MB (50.75%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -3,22 +3,21 @@ class Solution:
         
         rows = len(board)
         columns = len(board[0])
-        
+
         def dfs(r,c,index):
             if index == len(word):
                 return True
             
-            if not (0 <= r < rows and 0 <= c < columns) or board[r][c] != word[index]:
+            if (not(0 <= r < rows) or not(0 <= c < columns) or board[r][c] != word[index]):
                 return False
             
             temp = board[r][c]
-            board[r][c] = '.'
-            
-            found = (
-                dfs(r+1, c, index+1) or
-                dfs(r-1, c, index+1) or
-                dfs(r, c+1, index+1) or
-                dfs(r, c-1, index+1)
+            board[r][c] = '*'
+
+            found = (dfs(r+1,c,index+1)
+            or dfs(r-1,c,index+1)
+            or dfs(r,c+1,index+1)
+            or dfs(r,c-1,index+1)
             )
 
             board[r][c] = temp

4c791b8dbb2e5aaea02b272b3bc90195840ff3be
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-02-27 23:18:56 -0700

    Time: 3565 ms (65.61%), Space: 19.4 MB (82.6%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -12,15 +12,14 @@ class Solution:
                 return False
             
             temp = board[r][c]
-            board[r][c] = '*'
+            board[r][c] = "*"
 
-            found = (dfs(r+1,c,index+1)
-            or dfs(r-1,c,index+1)
-            or dfs(r,c+1,index+1)
-            or dfs(r,c-1,index+1)
-            )
+            found = (dfs(r+1, c, index+1) or
+            dfs(r-1, c, index+1) or dfs(r, c+1, index+1) or
+            dfs(r, c-1, index+1))
 
             board[r][c] = temp
+
             return found
 
         for r in range(rows):

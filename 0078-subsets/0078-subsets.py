119b8812cc936d1c151ae21a76b8f15ee1655e9b
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-03-01 12:02:58 -0700

    Time: 0 ms (100%), Space: 19.4 MB (61.72%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -7,13 +7,14 @@ class Solution:
         result.append(temp[:])
 
         def backtrack(index):
-            if index >= len(nums) or len(temp) > len(nums):
+            if index >= len(nums) or len(temp) == len(nums):
                 return
             
             backtrack(index+1)
 
             temp.append(nums[index])
             result.append(temp[:])
+
             backtrack(index+1)
             temp.pop()
         

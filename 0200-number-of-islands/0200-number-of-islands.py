222df0e38c34abb66ef5136d3fc6ed5e8ca7cc91
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-03-01 12:12:55 -0700

    Time: 273 ms (17.95%), Space: 27 MB (15.16%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -11,16 +11,18 @@ class Solution:
             queue = deque()
             queue.append((r,c))
             visited.add((r,c))
+
             directions = [(0,1), (0,-1), (1,0), (-1,0)]
 
             while queue:
                 row, column = queue.popleft()
+
                 for dr, dc in directions:
                     nr, nc = row + dr, column + dc
 
-                    if 0 <= nr < rows and 0 <= nc < columns and grid[nr][nc] == "1" and (nr, nc) not in visited:
-                        queue.append((nr,nc))
+                    if 0 <= nr < rows and 0 <= nc < columns and grid[nr][nc] == "1" and (nr,nc) not in visited:
                         visited.add((nr,nc))
+                        queue.append((nr,nc))
 
         for r in range(rows):
             for c in range(columns):

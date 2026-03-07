cfe140b88c930bd8595a585a8b4a3369eba2306e
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-03-07 14:48:10 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -0,0 +1,26 @@
+import heapq
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+
+        graph = defaultdict(list)
+
+        for u,v,time in times:
+            graph[u].append((v, time))
+        
+        min_times = {}
+        min_heap = [(0,k)] # distance from source to node, node
+
+        while min_heap:
+            time_k_to_i, i = heapq.heappop(min_heap)
+            if i in min_times:
+                continue
+            min_times[i] = time_k_to_i
+
+            for neighbor, neighbor_time in graph[i]:
+                if neighbor not in min_times:
+                    heapq.heappush(min_heap, (neighbor_time + time_k_to_i, neighbor))
+        
+        if len(min_times) == len(graph):
+            return max(min_times.values())
+        else:
+            return -1

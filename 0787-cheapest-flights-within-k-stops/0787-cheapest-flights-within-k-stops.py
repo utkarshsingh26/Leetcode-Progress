c0c26222b0addf1ab0e8130cacf0f052954c199a
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-03-07 19:13:29 -0700

    Time: 27 ms (45.92%), Space: 21.1 MB (30.42%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -8,7 +8,7 @@ class Solution:
             graph[source].append((destination, price))
         
         best = {} # (node, flights_used) = cost
-        min_heap = [(0,src,0)] # cost, node, flights_used
+        min_heap = [(0, src, 0)] # cost, node, flights_used
 
         while min_heap:
             cost, node, flights_used = heapq.heappop(min_heap)
@@ -19,14 +19,14 @@ class Solution:
             if flights_used == k+1:
                 continue
             
-            for neighbor, neighbor_cost in graph[node]:
-                new_cost = cost + neighbor_cost
+            for neighor, neigbhor_cost in graph[node]:
+                new_cost = cost + neigbhor_cost
                 new_flights_used = flights_used + 1
 
-                state = (neighbor, new_flights_used)
-
+                state = (neighor, new_flights_used)
+                
                 if state not in best or new_cost < best[state]:
                     best[state] = new_cost
-                    heapq.heappush(min_heap, (new_cost, neighbor, new_flights_used))
+                    heapq.heappush(min_heap, (new_cost, neighor, new_flights_used))
         
         return -1

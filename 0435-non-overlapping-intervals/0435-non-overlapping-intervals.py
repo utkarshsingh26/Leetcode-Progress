160db96bcc8bf55a5b11b6fe5a3962840860c75d
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-03-07 18:41:09 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -3,14 +3,12 @@ class Solution:
         
         intervals.sort(key=lambda x:x[1])
         count = 0
-        start = intervals[0][0]
         end = intervals[0][1]
 
         for interval in intervals[1:]:
             if not (end <= interval[0]):
                 count += 1
             else:
-                start = interval[0]
                 end = interval[1]
         
         return count

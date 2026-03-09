52788f201c403b53e7dbeaff862b99bfca3d0027
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-03-08 22:27:27 -0700

    Time: 1034 ms (5.01%), Space: 27.9 MB (38.4%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -0,0 +1,39 @@
+class Solution:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        
+        rows = len(grid)
+        columns = len(grid[0])
+        directions = [(0,1), (0,-1), (1,0), (-1,0)]
+        min_heap = [(0,0,0,0)] # steps, row, column, obstacles_used
+        visited = set()
+
+        while min_heap:
+            steps, row, column, obstacles_used = heapq.heappop(min_heap)
+
+            if row == rows-1 and column == columns-1:
+                return steps
+            
+            if obstacles_used > k:
+                continue
+            
+            if (row, column, obstacles_used) in visited:
+                continue
+            
+            visited.add((row, column, obstacles_used))
+
+            for dr, dc in directions:
+                nr, nc = row + dr, column + dc
+
+                if 0 <= nr < rows and 0 <= nc < columns and (nr,nc) not in visited:
+                    new_steps = steps + 1
+
+                    #blow through obstacle
+                    if grid[nr][nc] == 1 and obstacles_used < k:
+                        new_obstacles_used = obstacles_used + 1
+                        heapq.heappush(min_heap, (new_steps, nr, nc, new_obstacles_used))
+
+                    #respect the obstacle
+                    if grid[nr][nc] == 0:
+                        heapq.heappush(min_heap, (new_steps, nr, nc, obstacles_used))
+        
+        return -1

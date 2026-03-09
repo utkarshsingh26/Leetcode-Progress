11ca3ed37e0eaf8f19937004d35c1cbbd5958b3d
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-03-09 16:57:15 -0700

    Time: 3 ms (98.91%), Space: 20.3 MB (28.85%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -7,9 +7,9 @@ class Solution:
         right = len(s)-1
 
         while left < right:
-            if not (s[left] == s[right]):
+            if s[left] != s[right]:
                 return False
             left += 1
             right -= 1
-        
+
         return True

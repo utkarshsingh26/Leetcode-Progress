9e8f6bf828794407ea1b1d6d0f846649b4b8552a
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-03-09 21:48:12 -0700

    Time: 31 ms (44.8%), Space: 20.2 MB (26.6%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -4,23 +4,21 @@ class Solution:
         
         rows = len(grid)
         columns = len(grid[0])
-        seen = set()
-        min_heap = [(grid[0][0],0,0)] # time, row, column
         directions = [(0,1), (0,-1), (1,0), (-1,0)]
+        min_heap = [(grid[0][0], 0, 0)] # time, row, column
         visited = set()
 
         while min_heap:
             time, row, column = heapq.heappop(min_heap)
 
+            if row == rows-1 and column == columns-1:
+                return time
+            
             if (row, column) in visited:
                 continue
             
             visited.add((row, column))
 
-            if row == rows-1 and column == columns-1:
-                return time
-            
-            
             for dr, dc in directions:
                 nr, nc = row + dr, column + dc
 

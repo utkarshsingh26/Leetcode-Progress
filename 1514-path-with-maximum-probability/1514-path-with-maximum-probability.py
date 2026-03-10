cb6bab811ea37e435ad53d4f9ed231b7a837e590
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-03-10 10:41:03 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -18,7 +18,7 @@ class Solution:
             if node == end_node:
                 return prob
             
-            if node in best and best[node] <= prob:
+            if node in best and best[node] >= prob:
                 continue
             
             best[node] = prob

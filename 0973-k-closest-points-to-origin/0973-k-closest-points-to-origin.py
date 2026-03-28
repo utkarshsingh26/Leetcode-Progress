a4baaa0e377ec05d252967a8b2e55081ef0bb3d0
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-03-28 14:55:03 -0700

    Time: 51 ms (82.37%), Space: 24.2 MB (88.95%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -2,18 +2,19 @@ import heapq
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         
-        def euclid_distance(x,y):
+        def euclideanDistance(x,y):
             return x*x + y*y
         
         heap = []
 
         for point in points:
-            distance = euclid_distance(point[0], point[1])
+            dist = euclideanDistance(point[0], point[1])
+
             if len(heap) < k:
-                heapq.heappush(heap, (-distance, point[0], point[1]))
+                heapq.heappush(heap, (-dist, point))
             else:
-                heapq.heappushpop(heap, (-distance, point[0], point[1]))
+                heapq.heappushpop(heap, (-dist, point))
         
-        return [(x,y) for z,x,y in heap]
+        return [point for _,point in heap]
         
 

f3a699d6fa6eba778c9bcea3c39a941bc8d283ae
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-06-07 14:36:11 -0700

    Time: 11 ms (84.06%), Space: 22 MB (66.92%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,12 +2,11 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
         graph = defaultdict(list)
-
-        for s in strs:
-            graph[''.join(sorted(s))].append(s)
-        
         result = []
 
+        for s in strs:
+            graph["".join(sorted(s))].append(s)
+        
         for key,val in graph.items():
             result.append(val)
         

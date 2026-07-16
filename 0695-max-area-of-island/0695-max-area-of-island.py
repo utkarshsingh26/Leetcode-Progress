3a2c52547b26b7bcc5454aace22be2184ee48b33
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-07-16 14:39:08 -0700

    Time: 19 ms (61.73%), Space: 19.5 MB (84.93%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -12,20 +12,22 @@ class Solution:
             queue = deque()
             queue.append((r,c))
             visited.add((r,c))
+
             directions = [(0,1), (0,-1), (1,0), (-1,0)]
 
             while queue:
                 row, column = queue.popleft()
+
                 for dr, dc in directions:
                     nr, nc = row + dr, column + dc
 
-                    if 0 <= nr < rows and 0 <= nc < columns and grid[nr][nc] == 1 and (nr, nc) not in visited:
-                        queue.append((nr, nc))
-                        visited.add((nr, nc))
+                    if 0 <= nr < rows and 0 <= nc < columns and grid[nr][nc] == 1 and (nr,nc) not in visited:
                         area += 1
+                        visited.add((nr,nc))
+                        queue.append((nr,nc))
             
             return area
-
+        
         for r in range(rows):
             for c in range(columns):
                 if grid[r][c] == 1 and (r,c) not in visited:

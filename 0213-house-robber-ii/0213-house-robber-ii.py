5690042d15e765a6d86a6ac893fe8e8900ecc74e
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-07-17 23:26:19 -0700

    Time: 0 ms (100%), Space: 19.3 MB (69.16%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,28 +1,21 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-
+        
         if len(nums) == 1:
             return nums[0]
         
         if len(nums) == 2:
             return max(nums[0], nums[1])
-        
-        def robbing(nums):
 
+        def rob(nums):
             dp = [0] * len(nums)
             dp[0] = nums[0]
             dp[1] = max(nums[0], nums[1])
+            print(dp)
+            for i in range(2, len(nums)):
+                dp[i] = max(dp[i-2] + nums[i], dp[i-1])
 
-            for i in range(2, len(dp)):
-                dp[i] = max(dp[i-1], dp[i-2] + nums[i])
-            
             return dp[-1]
-        
-        length_of_houses = len(nums)
-
-        scenario1 = robbing(nums[1:])
-        scenario2 = robbing(nums[:length_of_houses-1])
-
-        return max(scenario1, scenario2)
 
 
+        return max(rob(nums[1:]), rob(nums[:-1]))

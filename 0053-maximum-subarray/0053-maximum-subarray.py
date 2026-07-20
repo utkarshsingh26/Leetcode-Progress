e2c3ae2223a52a526a297d71db3de97eced20ea7
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-07-20 16:27:59 -0700

    Time: 31 ms (73.22%), Space: 31.5 MB (43.49%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -6,8 +6,7 @@ class Solution:
 
         for i in range(len(nums)):
             summ += nums[i]
-            max_sum = max(max_sum, summ)
-
+            max_sum = max(summ, max_sum)
             if summ < 0:
                 summ = 0
         

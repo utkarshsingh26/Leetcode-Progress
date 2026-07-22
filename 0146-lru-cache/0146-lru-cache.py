3894b61d4948d711f0fbc6306266355bec9144da
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-07-22 10:15:53 -0700

    Time: 124 ms (54.05%), Space: 78.7 MB (16.22%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,71 +1,84 @@
 class Node:
-    def __init__(self, key, val):
+
+    def __init__(self, key, value):
         self.key = key
-        self.val = val
-        self.prev = None
+        self.value = value
         self.next = None
+        self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
+        self.cache = {}
+        self.capacity = capacity
         self.start = Node(0,0)
         self.end = Node(0,0)
         self.start.next = self.end
         self.end.prev = self.start
 
-        self.capacity = capacity
-        self.cache = {}
-    
-    def _remove_from_list(self, node):
-        previous_node = node.prev
-        next_node = node.next
+    def remove_from_spot(self, node):
+        before = node.prev
+        after = node.next
 
-        node.prev = None
+        before.next = after
+        after.prev = before
+
         node.next = None
+        node.prev = None
 
-        previous_node.next = next_node
-        next_node.prev = previous_node
-    
-    def _add_to_front(self, node):
-        current_top = self.start.next
+    def move_to_front(self, node):
+        curr_first = self.start.next
 
         self.start.next = node
         node.prev = self.start
-
-        node.next = current_top
-        current_top.prev = node
+        curr_first.prev = node
+        node.next = curr_first
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
-        
         node = self.cache[key]
-        self._remove_from_list(node)
-        self._add_to_front(node)
-        return node.val
+        self.remove_from_spot(node)
+        self.move_to_front(node)
+        return node.value
+    
+    def remove_from_end(self):
+        curr_last = self.end.prev
+        curr_second_last = curr_last.prev
+
+        curr_last.prev = None
+        curr_last.next = None
+
+        curr_second_last.next = self.end
+        self.end.prev = curr_second_last
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            node = self.cache[key]
-            node.val = value
-            self._remove_from_list(node)
-            self._add_to_front(node)
+        if len(self.cache) < self.capacity:
+            if key not in self.cache:
+                node = Node(key, value)
+                self.cache[key] = node
+                self.move_to_front(node)
+            else:
+                node = self.cache[key]
+                node.value = value
+                self.remove_from_spot(node)
+                self.move_to_front(node)
         else:
-            if len(self.cache) < self.capacity:
-                node = Node(key,value)
-                self.cache[key] = node
-                self._add_to_front(node)
-            elif len(self.cache) == self.capacity:
-                last_node = self.end.prev
-                self._remove_from_list(last_node)
-                del self.cache[last_node.key]
+            if key not in self.cache:
+                curr_last = self.end.prev
+                key_old = curr_last.key
+                del self.cache[key_old]
+                self.remove_from_end()
 
                 node = Node(key,value)
                 self.cache[key] = node
-                self._add_to_front(node)
+                self.move_to_front(node)
 
-
-
+            else:
+                node = self.cache[key]
+                node.value = value
+                self.remove_from_spot(node)
+                self.move_to_front(node)
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)

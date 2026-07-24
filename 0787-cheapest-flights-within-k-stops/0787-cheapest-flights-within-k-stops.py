7b8da23bd26b5c0d5ae43bf2f33beb3e162ffe8a
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-07-23 23:27:50 -0700

    Time: 81 ms (26.35%), Space: 22.6 MB (6.18%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,16 +1,17 @@
+import heapq
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         
         graph = defaultdict(list)
 
-        for source, destination, price in flights:
-            graph[source].append((destination, price))
-        
-        min_heap = [(0,src,0)] # cost, node, stops
+        for u,v,w in flights:
+            graph[u].append((v,w))
+
+        heap = [(0, src, 0)] # cost, node, stops
         best = {} # (node, stops) = cost
 
-        while min_heap:
-            cost, node, stops = heapq.heappop(min_heap)
+        while heap:
+            cost, node, stops = heapq.heappop(heap)
 
             if node == dst:
                 return cost
@@ -18,14 +19,14 @@ class Solution:
             if stops > k:
                 continue
             
-            if (node, stops) in best and best[(node, stops)] <= cost:
+            if (node,stops) in best and best[(node,stops)] <= cost:
                 continue
             
-            best[(node, stops)] = cost
+            best[(node,stops)] = cost
 
-            for neighbor, neighbor_cost in graph[node]:
-                new_cost = cost + neighbor_cost
+            for neighbor in graph[node]:
+                new_cost = cost + neighbor[1]
                 new_stops = stops + 1
-                heapq.heappush(min_heap, (new_cost, neighbor, new_stops))
+                heapq.heappush(heap, (new_cost, neighbor[0], new_stops))
         
         return -1

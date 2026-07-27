53e94992ca4feb191ff6c20f210dac6c737fb045
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-07-27 10:53:43 -0700

    Time: 363 ms (14.52%), Space: 21.2 MB (77.07%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -8,23 +8,21 @@ class Solution:
             graph[u].append((v,w))
         
         heap = [(0,k)] # time, node
-        min_times = {}
+        min_times = {} # node = time
 
         while heap:
             time, node = heapq.heappop(heap)
-            
+
             if node in min_times:
                 continue
             
             min_times[node] = time
 
             for neighbor in graph[node]:
-                if neighbor[0] not in min_times:
-                    new_time = time + neighbor[1]
-                    heapq.heappush(heap, (new_time, neighbor[0]))
+                new_time = time + neighbor[1]
+                heapq.heappush(heap, (new_time, neighbor[0]))
         
         if len(min_times) != n:
             return -1
         
         return max(min_times.values())
-        

a18d24d77c17489b26eaa08f0fc7c79807f26dbd
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-07-28 11:34:19 -0700

    Time: 42 ms (34.2%), Space: 19.4 MB (53.11%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -6,23 +6,23 @@ class Solution:
         
         count_s1 = Counter(s1)
 
-        k = len(s1)
-        window = s2[:k]
+        window = s2[:len(s1)]
         count_window = Counter(window)
 
-        if count_s1 == count_window:
+        if count_window == count_s1:
             return True
         
-        for right in range(k, len(s2)):
-            
+        left = 0
+        for right in range(len(s1), len(s2)):
             if s2[right] in count_window:
                 count_window[s2[right]] += 1
             else:
                 count_window[s2[right]] = 1
             
-            count_window[s2[right-k]] -= 1
+            count_window[s2[left]] -= 1
+            left += 1
 
-            if count_s1 == count_window:
+            if count_window == count_s1:
                 return True
-        
+
         return False

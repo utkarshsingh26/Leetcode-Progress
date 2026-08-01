feda88a6759cbd6c12aeea17be4cb0ea34b14f9c
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-08-01 12:43:29 -0700

    Time: 74 ms (90.06%), Space: 19.6 MB (50.4%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,16 +3,16 @@ class Solution:
         
         longest = 0
         left = 0
-        count = {}
+        window = {}
         max_freq = 0
 
         for right in range(len(s)):
-            count[s[right]] = count.get(s[right], 0) + 1
+            window[s[right]] = window.get(s[right], 0) + 1
 
-            max_freq = max(max_freq, count[s[right]])
+            max_freq = max(max_freq, window[s[right]])
 
-            while (right - left + 1) - max_freq > k:
-                count[s[left]] -= 1
+            if (right - left + 1) - max_freq > k:
+                window[s[left]] = window.get(s[left], 0) - 1
                 left += 1
             
             longest = max(longest, (right - left + 1))

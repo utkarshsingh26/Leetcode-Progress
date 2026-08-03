785fe7f8c0f28194d0e4ec6aa00d6d329e9fea80
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2026-08-03 15:06:07 -0700

    Time: 3 ms (72.64%), Space: 19.3 MB (52.12%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -6,7 +6,7 @@ class Solution:
         dp = [False] * (len(s) + 1)
         dp[0] = True
 
-        for i in range(1, len(s) + 1):
+        for i in range(1, len(s)+1):
             for j in range(i):
                 if dp[j] and s[j:i] in words:
                     dp[i] = True
